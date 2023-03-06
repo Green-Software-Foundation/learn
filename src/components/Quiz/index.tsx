7fb90cf4b831641020596fd5899ef9ae754d5f28
Osama Jandali <osamajandaly@gmail.com>
2023-03-06 04:15:58 +0200

    Add icons to quiz answers

--- src/components/Quiz/index.tsx
+++ src/components/Quiz/index.tsx
@@ -43,6 +43,8 @@ function QuizItem({ question, answers, chosenAnswer }: QuizItem) {
                 style={{ borderColor: bgColor }}
                 key={index}
               >
+                {/* Check mark for correct answer and cross for wrong one */}
+
                 <input
                   className={styles.radio_btn}
                   type="radio"
@@ -52,6 +54,37 @@ function QuizItem({ question, answers, chosenAnswer }: QuizItem) {
                   required
                 />
                 {answer.text}
+
+                {isChosen && answer.isCorrect && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="var(--ifm-color-success)"
+                    width={24}
+                    height={24}
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )}
+                {isChosen && !answer.isCorrect && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="var(--ifm-color-danger)"
+                    width={24}
+                    height={24}
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )}
               </label>
             );
           })}
