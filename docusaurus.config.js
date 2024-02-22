1506c85487e13a308d5cda52c22c54a7cb5fd7b8
Russell Trow <russell@dockleafdigital.com>
2024-02-22 13:43:34 +0000

    Updated Japanese locale code from "ja" to "ja-JP"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   deploymentBranch: "gh-pages",
   i18n: {
     defaultLocale: "en",
-    locales: ["en","it","zh-CN","ja"]
+    locales: ["en","it","zh-CN","ja-JP"]
   },
 
   presets: [
